ca78d3477d218f816eec82127acc852df7355e2c
Mindful Moments <zekereynolds313@gmail.com>
2024-04-15 15:46:15 -0400

    fixed Sleep meditation activity and linked appropriately

--- app/src/main/java/com/example/mindfulmoments/SleepMeditation.java
+++ app/src/main/java/com/example/mindfulmoments/SleepMeditation.java
@@ -1,2 +1,24 @@
-package com.example.mindfulmoments;public class SleepMeditation {
+package com.example.mindfulmoments;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SleepMeditation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_sleep_meditation);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
 }
