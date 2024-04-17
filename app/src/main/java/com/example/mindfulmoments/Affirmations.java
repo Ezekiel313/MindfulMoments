ba887b1a77bb1189736a39355371d316fe675e3e
hwrighx <heyheyhjw2012@gmail.com>
2024-04-17 14:16:29 -0400

    updates to affirmations

--- app/src/main/java/com/example/mindfulmoments/Affirmations.java
+++ app/src/main/java/com/example/mindfulmoments/Affirmations.java
@@ -1,13 +1,7 @@
 package com.example.mindfulmoments;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
 import android.widget.TextView;
 import android.os.Handler;
 
